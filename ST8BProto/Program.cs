49c2e0c33e3920c92c5e77096c5a9c0e1f172496
sat <ko_nishino@tr-h.co.jp>
2022-09-06 17:18:34 +0900

    プロジェクト ファイルを追加します。

---
+++ ST8BProto/Program.cs
@@ -0,0 +1,203 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+public class PC2
+{
+    public void Pool()
+    {
+        //
+    }
+}
+
+public class PC3
+{
+    public void T1000Supply1()
+    {
+    }
+
+    public void T1000Supply2()
+    {
+    }
+
+    public void T10000Waste1()
+    {
+    }
+
+    public void T10000Waste2()
+    {
+    }
+
+    public void Beads()
+    {
+    }
+
+    public void Shaker1()
+    {
+    }
+
+    public void Shaker2()
+    {
+    }
+
+    public void JoseiJokyo()
+    {
+    }
+
+    public void Senjo()
+    {
+    }
+
+}
+
+public class PC4
+{
+    public void T300Supply1()
+    {
+    }
+
+    public void T300Supply2()
+    {
+    }
+
+    public void T3000Waste1()
+    {
+    }
+
+    public void T3000Waste2()
+    {
+    }
+
+    public void PCR96Supply()
+    {
+    }
+
+    public void SP96Supply()
+    {
+    }
+
+
+    public void Isai()
+    {
+    }
+
+    public void JoseiJokyo()
+    {
+    }
+
+    public void Maesyori()
+    {
+    }
+
+    public void PCRSiyaku()
+    {
+    }
+
+    public void Kentai()
+    {
+    }
+}
+
+public class PC5
+{
+    public void Sealer1()
+    {
+    }
+
+    public void Sealer2()
+    {
+    }
+
+    public void ThermalCycler()
+    {
+    }
+
+    public void PCR1()
+    {
+    }
+}
+
+public class PC6
+{
+    public void PCR2()
+    {
+    }
+}
+
+
+public class Robot2
+{
+    public void PC2ToPC3Shaker()
+    {
+    }
+
+}
+
+public class Robot12
+{
+    public void PC3ShakerToPC3Magnet()
+    {
+    }
+}
+
+public class Robot1
+{
+    public void PC3MagnetToPC3Normal()
+    {
+    }
+
+    public void PC3NormalToPC4Magnet()
+    {
+    }
+
+    public void PC4MagnetToPC4Normal()
+    {
+    }
+
+    public void PC5MagnetToPC4Haiki()
+    {
+    }
+
+    public void PC4NormalToPC5Sealer1()
+    {
+    }
+
+    public void PC5Sealer1ToPC5ATC()
+    {
+    }
+
+    public void PC5ATCToPC4Normal()
+    {
+    }
+
+    public void PC4NormalToPC4Haiki()
+    {
+    }
+
+    public void PC4CoolerToPC5Sealer2()
+    {
+    }
+
+    public void PC5Sealer2ToPC5PCR1()
+    {
+    }
+
+    public void PC5Sealer2ToPC5PCR2()
+    {
+    }
+
+    public void PC5PCR1ToPC5Supply3()
+    {
+    }
+
+    public void PC5PCR2ToPC5Supply3()
+    {
+    }
+
+
+}
+
+public class BuntyuProcess
+{
+    PC2 PC2 = new();
+
+}
